db6ff9bd6e1436ed1f939e0957ef93db0693eb34
Yoni Weisberg <yoniwg@gmail.com>
2017-11-08 04:13:04 +0200

    1

--- CurrencyPL/ColorOfChangeConverter.cs
+++ CurrencyPL/ColorOfChangeConverter.cs
@@ -9,18 +9,11 @@ namespace CurrencyPL
 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            var redColor = System.Drawing.Color.Red;
-            var greenColor = System.Drawing.Color.Green;
-            var yellowColor = System.Drawing.Color.Yellow;
-            var blackColor = System.Drawing.Color.Black;
             decimal? decValue = value as decimal?;
             if (!decValue.HasValue)
             {
-
-                return blackColor;
+                return "Black";
             }
-            var desiredColor = (decValue > 0) ? greenColor : (decValue < 0) ? redColor : yellowColor;
-            //return Color.FromArgb(desiredColor.A, desiredColor.R, desiredColor.G, desiredColor.B);
             return (decValue > 0) ? "Green" : (decValue < 0) ? "Red" : "Yellow";
         }
 
